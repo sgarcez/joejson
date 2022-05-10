59352c9ef4d194fe1b1e36b32f0ecc5a88f67c2c
Sergio Garcez <garcez.sergio@gmail.com>
2022-05-10 11:28:00 +0100

    Use pointers for geometries
    Signed-off-by: Sergio Garcez <garcez.sergio@gmail.com>

--- geometrycollection.go
+++ geometrycollection.go
@@ -2,7 +2,6 @@ package joejson
 
 import (
 	"encoding/json"
-	"fmt"
 )
 
 // GeometryTypeGeometryCollection is the value for a GeometryCollection's 'type' member.
@@ -12,32 +11,32 @@ const GeometryTypeGeometryCollection = "GeometryCollection"
 type GeometryCollection []GeometryCollectionMember
 
 // AppendPoint appends a Point to the collection.
-func (g GeometryCollection) AppendPoint(m Point) GeometryCollection {
+func (g GeometryCollection) AppendPoint(m *Point) GeometryCollection {
 	return append(g, GeometryCollectionMember{m})
 }
 
 // AppendMultiPoint appends a MultiPoint to the collection.
-func (g GeometryCollection) AppendMultiPoint(m MultiPoint) GeometryCollection {
+func (g GeometryCollection) AppendMultiPoint(m *MultiPoint) GeometryCollection {
 	return append(g, GeometryCollectionMember{m})
 }
 
 // AppendLineString appends a LineString to the collection.
-func (g GeometryCollection) AppendLineString(m LineString) GeometryCollection {
+func (g GeometryCollection) AppendLineString(m *LineString) GeometryCollection {
 	return append(g, GeometryCollectionMember{m})
 }
 
 // AppendMuliLineString appends a MultiLineString to the collection.
-func (g GeometryCollection) AppendMuliLineString(m MultiLineString) GeometryCollection {
+func (g GeometryCollection) AppendMuliLineString(m *MultiLineString) GeometryCollection {
 	return append(g, GeometryCollectionMember{m})
 }
 
 // AppendPolygon appends a Polygon to the collection.
-func (g GeometryCollection) AppendPolygon(m Polygon) GeometryCollection {
+func (g GeometryCollection) AppendPolygon(m *Polygon) GeometryCollection {
 	return append(g, GeometryCollectionMember{m})
 }
 
 // AppendMultiPolygon appends a MultiPolygon to the collection.
-func (g GeometryCollection) AppendMultiPolygon(m MultiPolygon) GeometryCollection {
+func (g GeometryCollection) AppendMultiPolygon(m *MultiPolygon) GeometryCollection {
 	return append(g, GeometryCollectionMember{m})
 }
 
@@ -56,15 +55,10 @@ func (g GeometryCollection) MarshalJSON() ([]byte, error) {
 func (g *GeometryCollection) UnmarshalJSON(b []byte) error {
 	var tmp struct {
 		Geometries []json.RawMessage `json:"geometries"`
-		Type       string            `json:"type"`
 	}
 
 	if err := json.Unmarshal(b, &tmp); err != nil {
 		return err
-	}
-
-	if tmp.Type != GeometryTypeGeometryCollection {
-		return fmt.Errorf("invalid type %q, expected %q", tmp.Type, GeometryTypeGeometryCollection)
 	}
 
 	for _, geom := range tmp.Geometries {
@@ -84,38 +78,38 @@ type GeometryCollectionMember struct {
 }
 
 // AsPoint casts the Geometry to a Point.
-func (g GeometryCollectionMember) AsPoint() (Point, bool) {
-	p, ok := g.geometry.(Point)
+func (g GeometryCollectionMember) AsPoint() (*Point, bool) {
+	p, ok := g.geometry.(*Point)
 	return p, ok
 }
 
 // AsMultiPoint casts the Geometry to a MultiPoint.
-func (g GeometryCollectionMember) AsMultiPoint() (MultiPoint, bool) {
-	p, ok := g.geometry.(MultiPoint)
+func (g GeometryCollectionMember) AsMultiPoint() (*MultiPoint, bool) {
+	p, ok := g.geometry.(*MultiPoint)
 	return p, ok
 }
 
 // AsLineString casts the Geometry to a LineString.
-func (g GeometryCollectionMember) AsLineString() (LineString, bool) {
-	p, ok := g.geometry.(LineString)
+func (g GeometryCollectionMember) AsLineString() (*LineString, bool) {
+	p, ok := g.geometry.(*LineString)
 	return p, ok
 }
 
 // AsMultiLineString casts the Geometry to a MultiLineString.
-func (g GeometryCollectionMember) AsMultiLineString() (MultiLineString, bool) {
-	p, ok := g.geometry.(MultiLineString)
+func (g GeometryCollectionMember) AsMultiLineString() (*MultiLineString, bool) {
+	p, ok := g.geometry.(*MultiLineString)
 	return p, ok
 }
 
 // AsPolygon casts the Geometry to a Polygon.
-func (g *GeometryCollectionMember) AsPolygon() (Polygon, bool) {
-	p, ok := g.geometry.(Polygon)
+func (g *GeometryCollectionMember) AsPolygon() (*Polygon, bool) {
+	p, ok := g.geometry.(*Polygon)
 	return p, ok
 }
 
 // AsMultiPolygon casts the Geometry to a MultiPolygon.
-func (g *GeometryCollectionMember) AsMultiPolygon() (MultiPolygon, bool) {
-	p, ok := g.geometry.(MultiPolygon)
+func (g *GeometryCollectionMember) AsMultiPolygon() (*MultiPolygon, bool) {
+	p, ok := g.geometry.(*MultiPolygon)
 	return p, ok
 }
 
@@ -130,17 +124,17 @@ func (g GeometryCollectionMember) MarshalJSON() ([]byte, error) {
 // Type is the type of the Geometry.
 func (g GeometryCollectionMember) Type() string {
 	switch g.geometry.(type) {
-	case Point:
+	case *Point:
 		return GeometryTypePoint
-	case MultiPoint:
+	case *MultiPoint:
 		return GeometryTypeMultiPoint
-	case LineString:
+	case *LineString:
 		return GeometryTypeLineString
-	case MultiLineString:
+	case *MultiLineString:
 		return GeometryTypeMultiLineString
-	case Polygon:
+	case *Polygon:
 		return GeometryTypePolygon
-	case MultiPolygon:
+	case *MultiPolygon:
 		return GeometryTypeMultiPolygon
 	default:
 		return ""
